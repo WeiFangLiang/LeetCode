4be2f324edc4db0d44f1b93ff6abf3c7d0fed88b
WeiFangLiang <1564209774@qq.com>
2019-07-23 12:04:01 +0800

    add 98

---
+++ easy/Solution98.java
@@ -0,0 +1,46 @@
+package com.LeetCode.easy;
+/**
+ * 验证二叉搜索树(不会)
+ * 		方法一、中序遍历二叉树
+ * 		方法二、递归
+ * 		注：空树返回 true
+ * @author WeiFangLiang
+ *
+ */
+public class Solution98 {
+	 public static boolean helper(TreeNode node, Integer lower, Integer upper) {
+		    if (node == null) return true;
+
+		    int val = node.val;
+		    if (lower != null && val <= lower) return false;
+		    if (upper != null && val >= upper) return false;
+
+		    if (! helper(node.right, val, upper)) return false;
+		    if (! helper(node.left, lower, val)) return false;
+		    return true;
+		  }
+
+	  public static boolean isValidBST(TreeNode root) {
+	    return helper(root, null, null);
+	  }
+		  
+	public static void main(String[] args) {
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node2 = new TreeNode(2);
+		node2.left = node1;
+		node2.right = node3;
+		
+		boolean bst = isValidBST(node2);
+		System.out.println(bst);
+	}
+}
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+     
+      TreeNode(int x){
+    	  val = x;
+    }
+ }
