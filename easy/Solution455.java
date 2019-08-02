d09c344ddf4e845b76b91099b944bbd6537b0de2
WeiFangLiang <1564209774@qq.com>
2019-08-02 17:12:11 +0800

    贪心算法--分饼干

---
+++ easy/Solution455.java
@@ -0,0 +1,27 @@
+package com.LeetCode.easy;
+
+import java.util.Arrays;
+
+/**
+ * 分发饼干 
+ * @author WeiFangLiang
+ *
+ */
+public class Solution455 {
+	public int findContentChildren(int[] g, int[] s) {
+		Arrays.parallelSort(g);
+		Arrays.parallelSort(s);
+		//1,2   1,2,3
+		int i = 0;//孩子数组的下标
+		int j = 0;//饼干数组的下标
+		while(i < g.length && j < s.length) {
+			if(g[i] <= s[j]) {
+				i++;
+				j++;
+			}else {
+				j++;
+			}
+		}
+		return i;
+	}
+}
