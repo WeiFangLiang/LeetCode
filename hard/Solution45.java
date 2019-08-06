00f169db15824c2170202d0696d3fa0b0c925275
WeiFangLiang <1564209774@qq.com>
2019-08-06 12:27:58 +0800

    贪心之跳跃游戏II

--- hard/Solution45.java
+++ hard/Solution45.java
@@ -8,8 +8,8 @@ package com.LeetCode.hard;
 public class Solution45 {
 	public int jump(int[] nums) {
 		if(nums.length < 2) return 0; //长度小于2，不跳跃
-		int currentMaxIndex = nums[0];//当前可达到的最远位置
-		int preMaxMaxIndex = nums[0];//遍历各个位置过程中，可达到的最远位置
+		int currentMaxIndex = nums[0];//当前可达到的最远位置，本质是nums[i]+i，最远位置 的nums的索引
+		int preMaxMaxIndex = nums[0];//遍历各个位置过程中，可达到的最远位置,nums[i]+i
 		int jumpMin = 1;
 		for(int i = 0;i < nums.length;i++) {
 			if(i > currentMaxIndex) {//若无法向前移动了，才跳跃
@@ -17,7 +17,7 @@ public class Solution45 {
 				currentMaxIndex = preMaxMaxIndex;//更新当前可达到的最远位置
 			}
 			if(preMaxMaxIndex < nums[i] + i) {
-				preMaxMaxIndex = nums[i] + i;
+				preMaxMaxIndex = nums[i] + i; //记录了0-i 之间的可达到的最远位置
 			}
 		}
 		return jumpMin;
