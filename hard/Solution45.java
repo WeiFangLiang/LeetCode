6d60b6cec39b5d6f7152c3484fc7486e57fe99b1
WeiFangLiang <1564209774@qq.com>
2019-08-06 12:02:57 +0800

    贪心之跳跃游戏II

---
+++ hard/Solution45.java
@@ -0,0 +1,25 @@
+package com.LeetCode.hard;
+/**
+ * 跳跃游戏II
+ * 		贪心：只在必须跳的时候跳
+ * @author WeiFangLiang
+ *
+ */
+public class Solution45 {
+	public int jump(int[] nums) {
+		if(nums.length < 2) return 0; //长度小于2，不跳跃
+		int currentMaxIndex = nums[0];//当前可达到的最远位置
+		int preMaxMaxIndex = nums[0];//遍历各个位置过程中，可达到的最远位置
+		int jumpMin = 1;
+		for(int i = 0;i < nums.length;i++) {
+			if(i > currentMaxIndex) {//若无法向前移动了，才跳跃
+				jumpMin++;
+				currentMaxIndex = preMaxMaxIndex;//更新当前可达到的最远位置
+			}
+			if(preMaxMaxIndex < nums[i] + i) {
+				preMaxMaxIndex = nums[i] + i;
+			}
+		}
+		return jumpMin;
+	}
+}
