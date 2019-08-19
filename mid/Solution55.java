0960f47283c326ffe40b353c1701a5366bc75fbe
WeiFangLiang <1564209774@qq.com>
2019-08-19 12:51:28 +0800

    flush 55

--- mid/Solution55.java
+++ mid/Solution55.java
@@ -1,8 +1,5 @@
 package com.LeetCode.mid;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 跳跃游戏
  * 		贪心：根据当前元素最远可跳的位置，那么就有很多跳的选择，
@@ -12,17 +9,23 @@ import java.util.List;
  */
 public class Solution55 {
 	public boolean canJump(int[] nums) {
-		List<Integer> index = new ArrayList<>();//index是最远可跳至的位置，是个数组
-		for(int i = 0;i < nums.length;i++) {
-			index.add(i + nums[i]); //index存储了nums数组每个元素可以跳到的最远位置的nums的索引
-		}
-		int jump = 0;//代表当前所处位置
-		int maxIndex = index.get(0);//maxIndex存储可以使下一次跳到最远位置的  位置
-		while(jump < index.size() && jump <= maxIndex) {//直到jump跳到数组尾部或jump超越了当前可以跳的最远位置
-			if(maxIndex < index.get(jump))  maxIndex = index.get(jump);//如果可以跳的更远，就更新maxIndex
+		int[] index = new int[nums.length]; //index是最远可跳至的位置，是个数组
+        for(int i = 0;i < nums.length;i++) {
+        	index[i] = i + nums[i];  //index存储了nums数组每个元素可以跳到的最远位置的nums的索引
+        }
+        int jump = 0;//代表当前所处位置
+        int maxIndex = index[0];//maxIndex存储可以使下一次跳到最远位置的  位置
+		 /**
+         * jump <= maxIndex 防止的是
+         * nums = [3,2,1,0,4]      index = [3,3,3,3,8] 
+         * 前四个位置最多到3号位置
+         * maxIndex = 3
+         * jump = 4 位置时，因为jump > maxIndex，走到了无法到达的位置，循环结束
+         */
+		while(jump < index.length && jump <= maxIndex) {//直到jump跳到数组尾部或jump超越了当前可以跳的最远位置
+			if(maxIndex < index[jump])  maxIndex = index[jump];//如果可以跳的更远，就更新maxIndex
 			jump++;//扫描jump
 		}
-		if(jump == index.size()) return true; //jump到达数组尾，返回真 
-		return false;
+		return jump == index.length;  //jump =  index.length 号元素，说明 index.length-1 即数组末尾元素是可达的
     }
 }
