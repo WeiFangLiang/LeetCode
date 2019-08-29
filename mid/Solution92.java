438825ac1821fbf2895d6101bf04ed0953452817
WeiFangLiang <1564209774@qq.com>
2019-08-29 17:48:34 +0800

    flush 92

--- mid/Solution92.java
+++ mid/Solution92.java
@@ -6,6 +6,7 @@ package com.LeetCode.mid;
  *
  */
 public class Solution92 {
+	//74% ，60%
 	public ListNode reverseBetween(ListNode head, int m, int n) {
 		 int change_len = n - m + 1; //计算需要逆置的节点个数
 		 ListNode pre_head = null; //指向第一个逆置节点的前驱节点
@@ -31,4 +32,53 @@ public class Solution92 {
 		 }
 		 return result;
    }
+	
+	 // 74%，69% 自己想的
+	 public static ListNode reverseBetween2(ListNode head, int m, int n) {
+		 if(head == null) return head;
+		 ListNode slow = head;
+		 ListNode pre = null; //逆置区间的前一个节点
+		 for(int i = 1;i < m;i++) {
+			 pre = slow;
+			 slow = slow.next;
+		 }//slow指向逆置区间的首节点
+		 ListNode fast = slow;
+		 ListNode end = slow;
+		 for(int i = 0;i < n-m;i++) {
+			 fast = fast.next;
+		 } // fast指向逆置区间的尾节点
+		 ListNode next = fast.next; //逆置区间的下一个节点
+		 ListNode pre_ni = null;
+		 while(slow != next) {
+			 ListNode next_ni = slow.next;
+			 slow.next = pre_ni;
+			 pre_ni = slow;
+			 slow = next_ni;
+		 }
+		 if(pre == null) {
+			 end.next = next;
+			 return fast;
+		 }else {
+			 pre.next = fast;
+			 end.next = next;
+			 return head;
+		 }
+	 }
+	 
+	 public static void main(String[] args) {
+		ListNode node5 = new ListNode(5);
+		ListNode node4 = new ListNode(4);
+		ListNode node3 = new ListNode(3);
+		ListNode node2 = new ListNode(2);
+		ListNode node1 = new ListNode(1);
+		node4.next = node5;
+		node3.next = node4;
+		node2.next = node3;
+		node1.next = node2;
+		ListNode head = reverseBetween2(node1, 2, 4);
+		while(head != null) {
+			System.out.print(head.val + " ");
+			head = head.next;
+		}
+	}
 }
