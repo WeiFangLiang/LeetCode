116a14efbbb31e9a1e47405bb18711b78b684602
WeiFangLiang <1564209774@qq.com>
2019-12-15 17:58:42 +0800

    add 283

--- easy/Solution283.java
+++ easy/Solution283.java
@@ -8,10 +8,28 @@ public class Solution283 {
         	if(nums[i] != 0) { //找到非零数
         		nums[j] = nums[i];//放到j位置上
         		if(i != j) {//如果不是一个位置的
-        			nums[i] = 0;//那就和交换一个意思
+        			nums[i] = 0;//那就和交换一个意思，i的值换到j上了，i需要置为0
         		}
         		j++;
         	}
         }
+    }
+	/**
+	 * 思路：
+    	遍历数组，无为0的元素移动数组前方，用index下标记录。
+    	遍历结束，对index值后的元素统一设为0
+
+	 * @param nums
+	 */
+	public void moveZeroes_2(int[] nums) {
+		int index = 0;
+		for(int num:nums){
+			if(num!=0){
+				nums[index++]=num;
+			}
+		}
+		while(index<nums.length){
+			nums[index++] = 0;
+		}
     }
 }
