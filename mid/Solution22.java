4eae6502dece82df976cf645dffa3a5970b6edba
WeiFangLiang <1564209774@qq.com>
2019-12-28 16:16:16 +0800

    flush 22

--- mid/Solution22.java
+++ mid/Solution22.java
@@ -11,14 +11,15 @@ import java.util.List;
  *
  */
 public class Solution22 {
+//	99.39%  98.87%
 	public static List<String> generateParenthesis(int n) {
 		//回溯思想，需要加强练习
 		List<String> result = new ArrayList<>();
-		generateOneByOne("",result,n,n);
+		generateOneByOne("",result,n,n);//最开始是空字符串，还剩n个左右括号
 		return result;
     }
 	/**
-	 * 
+	 * 其实就是给你2n个格子，问你有效括号的组合数，第几层递归，就是处理第几个格子上的括号，判断放左还是放右
 	 * @param sublist
 	 * @param result  结果集
 	 * @param left  还剩多少左括号
@@ -29,10 +30,12 @@ public class Solution22 {
 			result.add(sublist);
 			return ;
 		}
-		if (left > 0) {
+		//每一层的递归，需要判断
+		if (left > 0) {//还有剩余的左括号，那这个格子就可以放左括号
 			generateOneByOne(sublist + "(", result, left-1, right);
 		}
-		if (right > left) {
+		if (right > left) {//只有剩余的右括号多于左括号时，这个格子才可以放右括号
+			//比如()()_ _，那第五层递归就不能放右括号，因为剩余的左右括号相等
 			generateOneByOne(sublist + ")", result, left, right-1);
 		}
 	}
