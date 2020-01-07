e63bfe5b068e07ab1f80f81e8a95ee1895d0aad3
WeiFangLiang <1564209774@qq.com>
2020-01-07 17:08:46 +0800

    flush 102

--- mid/Solution102.java
+++ mid/Solution102.java
@@ -14,7 +14,7 @@ import java.util.Queue;
  */
 public class Solution102 {
 	public List<List<Integer>> levelOrder(TreeNode root) {
-		//BFS
+		//方法1：迭代--BFS	   98.7%  43.8%
 		List<List<Integer>> res = new ArrayList<>();
 		if(root == null)  return res;
 		Queue<TreeNode> q = new LinkedList<>();
@@ -34,4 +34,26 @@ public class Solution102 {
 		}
 		return res;
 	}
+	//方法2：深度优先 DFS	 98.7%  50%
+	public List<List<Integer>> levelOrder2(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+		if(root == null)  return res;
+		DFS(root, 0, res);
+	    return res;
+	}
+	private void DFS(TreeNode root, int level, List<List<Integer>> res) {
+        if (root == null) return;
+        /**
+         * 0层----res是空的，所以new一个ArrayList
+         * 1层----res有一个ArrayList(保存0层结果的)，需要再new一个ArrayList
+         * 2层----res有两个ArrayList(保存0,1层结果的)，需要再new一个ArrayList
+         */
+        if (res.size() == level) {//说明还缺一个保留level层结果的ArrayList
+        	res.add(new ArrayList<>());
+        }
+        //当前值加入当前层
+        res.get(level).add(root.val);
+        DFS(root.left, level + 1, res);
+        DFS(root.right, level + 1, res);
+    }
 }
