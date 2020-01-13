9029ef05ebcd67132a387d9afbe1b1d6fef54f78
WeiFangLiang <1564209774@qq.com>
2020-01-13 12:25:22 +0800

    flush 69

--- easy/Solution69.java
+++ easy/Solution69.java
@@ -7,22 +7,29 @@ package com.LeetCode.easy;
  */
 public class Solution69 {
 	public int mySqrt(int x) {
-		//二分查找
-        if (x == 0 || x == 1) return x;
-        int l = 1;
-        int r = x;
-        int res = -1;
-        while(l <= r) {
-        	int m = (l+r)/2;
-        	if(m == x/m) {
-        		return m;
-        	}else if(m > x/m) {
-        		r = m-1;
-        	}else {
-        		l = m+1;
-        		res = m;
-        	}
+		//方法1：二分法： 80%   75%
+		if (x == 1 || x == 0) return x;
+        int start = 1;
+        int end = x / 2 + 1; //最终结果一定是在 start 和 end 之间
+        int mid = 0;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            //防止越界
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) return mid;
+            if (mid > x / mid) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
         }
-        return res;
+        return 99999;//最后这个随便return
     }
+	//方法2：牛顿迭代法	80%  79%
+	public int mySqrt2(int x) {
+		long r = x;
+		while(r * r > x) { //循环结束时，说明 r * r 第一次 <= x，也就是最终结果
+			r = (r + x / r) / 2;
+		}
+		return (int)r;
+	}
 }
