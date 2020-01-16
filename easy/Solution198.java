056c198b16e99b766db847a3e3fa6c322f4e98dc
WeiFangLiang <1564209774@qq.com>
2020-01-16 20:23:44 +0800

    flush 198

--- easy/Solution198.java
+++ easy/Solution198.java
@@ -5,16 +5,17 @@ package com.LeetCode.easy;
  *
  */
 public class Solution198 {
+	//100%  90%
 	public int rob(int[] nums) {
-		//牧码啦大佬的解法
+		if(nums == null || nums.length == 0) return 0;
 		int len = nums.length;
-        if(len == 0) return 0;          
-        int[] dp = new int[len + 1];
-        dp[0] = 0;
-        dp[1] = nums[0]; //dp[i]表示 i 号房屋的最优解
-        for(int i = 2; i <= len; i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
-        }
-        return dp[len];
+		int[][] dp = new int[len][2];
+		dp[0][0] = 0;
+		dp[0][1] = nums[0];
+		for(int i = 1;i < len;i++) {
+			dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+			dp[i][1] = dp[i - 1][0] + nums[i];
+		}
+		return Math.max(dp[len - 1][0], dp[len - 1][1]);
     }
 }
